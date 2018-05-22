f3cc5d5e75ceb17c5ac4550644ace96565e15e61
Serdar Dogruyol <dogruyolserdar@gmail.com>
2018-05-22 16:55:30 +0300

    Add JSON.mapping cookbook

---
+++ _cookbook/code/json-mapping/app.cr
@@ -0,0 +1,17 @@
+require "kemal"
+require "json"
+
+# You can use JSON.mapping to directly create an object from JSON
+class User
+  JSON.mapping(
+    username: String,
+    password: String,
+  )
+end
+
+post "/" do |env|
+  user = User.from_json env.request.body.not_nil!
+  {username: user.username, password: user.password}.to_json
+end
+
+Kemal.run
---
+++ _cookbook/json_mapping.md
@@ -0,0 +1,10 @@
+---
+layout: doc
+title: JSON API Recipe
+---
+
+```ruby
+{% include_relative code/json-mapping/app.cr %}
+```
+
+[Source Code](https://github.com/kemalcr/kemalcr.com/tree/master/_cookbook/code/json-mapping)
