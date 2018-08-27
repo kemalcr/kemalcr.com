9e0a97b0f65c2f3e6239dc65b2ab96c71e88f32d
Adam Coquery <adam@minimalism.love>
2018-08-27 11:36:07 +0200

    Update File Upload (cookbook & guide)

--- _cookbook/code/file-upload/app.cr
+++ _cookbook/code/file-upload/app.cr
@@ -1,17 +1,18 @@
 require "kemal"
 
 post "/upload" do |env|
-  env.params.files.each do |file|
-    filename = file.filename
-
+  HTTP::FormData.parse(env.request) do |upload|
+    filename = upload.filename
+  # Be sure to check if file.filename is not empty otherwise it'll raise a compile time error
     if !filename.is_a?(String)
-      "No filename included in upload"
+      p "No filename included in upload"
     else
       file_path = ::File.join [Kemal.config.public_folder, "uploads/", filename]
       File.open(file_path, "w") do |f|
-      IO.copy(file.tmpfile, f)
+        IO.copy(upload.body, f)
+      end
+      "Upload ok"
     end
-    "Upload OK"
   end
 end
 
