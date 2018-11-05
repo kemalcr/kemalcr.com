8c776565b7a6f5d395a49c6b469145fa41331971
Anton Maminov <anton.linux@gmail.com>
2018-11-05 22:17:22 +0200

    change File Upload Recipe

--- _cookbook/code/file-upload/app.cr
+++ _cookbook/code/file-upload/app.cr
@@ -1,19 +1,12 @@
 require "kemal"
 
 post "/upload" do |env|
-  HTTP::FormData.parse(env.request) do |upload|
-    filename = upload.filename
-  # Be sure to check if file.filename is not empty otherwise it'll raise a compile time error
-    if !filename.is_a?(String)
-      p "No filename included in upload"
-    else
-      file_path = ::File.join [Kemal.config.public_folder, "uploads/", filename]
-      File.open(file_path, "w") do |f|
-        IO.copy(upload.body, f)
-      end
-      "Upload ok"
-    end
+  file = env.params.files["image1"].tempfile
+  file_path = ::File.join [Kemal.config.public_folder, "uploads/", File.basename(file.path)]
+  File.open(file_path, "w") do |f|
+    IO.copy(file, f)
   end
+  "Upload ok"
 end
 
 Kemal.run
