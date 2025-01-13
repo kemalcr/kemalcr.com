b606ca234eaa6a0613a7a09cfabe7978404c8d69
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2025-01-13 13:50:14 +0300

    Improve samples with comments

--- _cookbook/code/json-api/app.cr
+++ _cookbook/code/json-api/app.cr
@@ -1,15 +1,67 @@
 require "kemal"
 require "json"
 
-# Define a route for the root path "/"
-get "/" do |env|
-  # Set response content type to JSON
-  # This tells clients to expect JSON data
+# Set JSON content type for all routes
+before_all do |env|
   env.response.content_type = "application/json"
+end
 
-  # Create a simple hash and convert it to JSON
-  # Returns a JSON string: {"name": "Serdar", "age": 27}
-  {name: "Serdar", age: 27}.to_json
+# In-memory storage for users
+USERS = [] of Hash(String, JSON::Any)
+
+# GET - List all users
+get "/users" do |env|
+  USERS.to_json
+end
+
+# GET - Get a specific user by index
+get "/users/:id" do |env|
+  id = env.params.url["id"].to_i
+  
+  if id < USERS.size
+    USERS[id].to_json
+  else
+    env.response.status_code = 404
+    {error: "User not found"}.to_json
+  end
+end
+
+# POST - Create a new user
+post "/users" do |env|
+  # Parse request body as JSON
+  user = JSON.parse(env.request.body.not_nil!.gets_to_end)
+  USERS << user.as_h
+  
+  env.response.status_code = 201
+  user.to_json
+end
+
+# PUT - Update a user
+put "/users/:id" do |env|
+  id = env.params.url["id"].to_i
+  
+  if id < USERS.size
+    # Parse request body as JSON
+    updated_user = JSON.parse(env.request.body.not_nil!.gets_to_end)
+    USERS[id] = updated_user.as_h
+    updated_user.to_json
+  else
+    env.response.status_code = 404
+    {error: "User not found"}.to_json
+  end
+end
+
+# DELETE - Remove a user
+delete "/users/:id" do |env|
+  id = env.params.url["id"].to_i
+  
+  if id < USERS.size
+    deleted_user = USERS.delete_at(id)
+    deleted_user.to_json
+  else
+    env.response.status_code = 404
+    {error: "User not found"}.to_json
+  end
 end
 
 # Start the Kemal web server
--- _includes/doc-sidebar.md
+++ _includes/doc-sidebar.md
@@ -1,7 +1,7 @@
 <div class="Sidebar">
   <div class="h3 Sidebar_title">Cookbooks</div>
   <nav class="Sidebar_nav">
-    {% assign sorted_pages = (site.cookbook | sort: 'order') %}
+    {% assign sorted_pages = (site.cookbook | sort: 'title') %}
     {% for post in sorted_pages %}
       <a href="{{ post.url | prepend: site.baseurl }}">{{ post.title }}</a>
     {% endfor %}
